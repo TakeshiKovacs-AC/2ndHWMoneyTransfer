b2d3d2f38b8ed6e333ca2ccc93c0a22d98529346
Takeshi Kovacs <antonbasket@list.ru>
2022-03-11 00:11:35 +1000

    Корректировка: убрал вывод минимальной суммы перевода, детально расписал снимаемые суммы

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,11 @@
 fun main() {
-    val amount = 466_900
+    val amount = 500_000
     val commPercentage = 0.0075
     val minCom = 3_500
     val minSum = minCom / commPercentage
-    println(minSum)
 
     val commission = if (amount > minSum.toInt()) amount * commPercentage else minCom
-    println(commission.toInt())
+    println("С вашего счёта будет списано ${amount + commission.toInt()} коп., " +
+            "из которых $amount коп. - сумма перевода, а ${commission.toInt()} коп. - сумма комиссии!")
 }
 
