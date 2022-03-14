fa6a502eb34053034f74a98fd7c0df308f90bdf0
Takeshi Kovacs <antonbasket@list.ru>
2022-03-14 14:59:38 +1000

    Корректировка2: расчёт реальной комиссии вынесен в переменную, скорректирован алгоритм вывода финальной суммы перевода

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,18 @@
 fun main() {
-    val amount = 500_000
+    val amount = 1_000_000
     val commPercentage = 0.0075
     val minCom = 3_500
-    val minSum = minCom / commPercentage
+//    val minSum = minCom / commPercentage
+    val realComm = amount * commPercentage
 
-    val commission = if (amount > minSum.toInt()) amount * commPercentage else minCom
-    println("С вашего счёта будет списано ${amount + commission.toInt()} коп., " +
-            "из которых $amount коп. - сумма перевода, а ${commission.toInt()} коп. - сумма комиссии!")
+//    val commission = if (amount > minSum.toInt()) amount * commPercentage else minCom
+//    println("С вашего счёта будет списано ${amount + commission.toInt()} коп., " +
+//            "из которых $amount коп. - сумма перевода, а ${commission.toInt()} коп. - сумма комиссии!")
+
+    val commission = if ((realComm).toInt() > minCom) realComm.toInt() else minCom
+    println("С вашего счёта будет списано ${amount + commission} коп., " +
+            "из которых $amount коп. - сумма перевода, а $commission коп. - сумма комиссии!")
+
+
 }
 
